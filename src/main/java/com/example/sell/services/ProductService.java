2b44ec62856d049b47bf04f85f650bfb352deee4
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-01-17 15:26:45 +0400

    Spring Boot #3

--- src/main/java/com/example/sell/services/ProductService.java
+++ src/main/java/com/example/sell/services/ProductService.java
@@ -1,38 +1,36 @@
 package com.example.sell.services;
 
 import com.example.sell.models.Product;
+import com.example.sell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
-
-    {
-        products.add(new Product(++ID,"AppleWatch", "Simple", 65000, "Samara", "Petr"));
-        products.add(new Product(++ID,"Apple Iphone", "Simple", 100000, "Samara", "Mars"));
-    }
-
-    public List<Product> listProducts() {
-        return products;
+    private final ProductRepository productRepository;
+    public List<Product> listProducts(String title) {
+        if (title != null) {
+            return productRepository.findByTitle(title);
+        }
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+       return productRepository.findById(id).orElse(null);
     }
 }
