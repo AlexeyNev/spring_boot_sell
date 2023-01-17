9982ecd0ae1bc26ca1ef9ebcaf85e3cb78fc73c2
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-01-17 12:33:24 +0400

    Spring Boot #2

---
+++ src/main/java/com/example/sell/services/ProductService.java
@@ -0,0 +1,38 @@
+package com.example.sell.services;
+
+import com.example.sell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+
+    {
+        products.add(new Product(++ID,"AppleWatch", "Simple", 65000, "Samara", "Petr"));
+        products.add(new Product(++ID,"Apple Iphone", "Simple", 100000, "Samara", "Mars"));
+    }
+
+    public List<Product> listProducts() {
+        return products;
+    }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+        return null;
+    }
+}
